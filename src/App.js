8d493d28ff8e2b4fe6847e9096985c5262835a89
kiran97171 <120737308+kiran97171@users.noreply.github.com>
2023-07-19 22:11:48 +0530

    new add file

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import Login from './Components/Login';
+import Signup from './Components/Signup';
+import "./Components/Components.css"
+import Bollywood from './Components/Bollywood';
+import Technology from './Components/Technology';
+import Hollywood from './Components/Hollywood';
+import Fitness from './Components/Fitness';
+
+const App = () => {
+  const [state,setState] = useState(true);
+  return (
+    <>
+    {state?<Login setState={setState}/>:<Signup setState={setState}/>}
+    <div className='buttons'>
+    <Bollywood/>
+    <Technology/>
+    <Hollywood/>
+    <Fitness/>
+
+    </div>
+    </>
+  )
+}
+
+export default App
